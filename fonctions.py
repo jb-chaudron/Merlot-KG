e6f711204a26b7045320a570610d6ee60306aa0d
Jean-Baptiste Chaudron <jean-baptiste.chaudron@ens-lyon.fr>
2021-12-03 16:26:20 +0100

    Update fonctions.py

--- fonctions.py
+++ fonctions.py
@@ -37,3 +37,29 @@ def get_ent(docc):
     print(out)
     print(set(subj))
     return subj, out
+
+from Levenshtein import ratio
+def NER_ext(phrases):
+    NE = [[j for j in i.ents] for i in phrases]
+    uni_NE, count_NE = np.unique(reduce(lambda a,b: a+b, [[y.text for y in x] for x in NE]),return_counts=True)
+    
+    return uni_NE,count_NE
+
+def synonyme(entite):
+    
+    ratio_list = np.array([[i,j,ratio(i,j)] for i,j in itr.product(entite,entite) if (not i==j) and((len(i) > 6) or (len(j) > 6))])
+    
+    mask_best = np.where(ratio_list[:,2].astype(float) <0.8, False, True )
+    
+    return ratio_list[mask_best]
+    """
+    df_in = pd.DataFrame(0,index=entite,columns=entite)
+    for i,j in itr.product(df_in.index,df_in.columns):
+        if i == j:
+            continue
+        df_in.loc[i,j] = ratio(i,j)
+    
+    max_list = []
+    for i in dff.sort_index(key=lambda x : dff.loc[x,:].max(),ascending=False).index:
+        j = list(dff.columns[dff[i]==dff[i].max()])[0]
+        max_list += [[i,j,dff.loc[i,j]]]"""
